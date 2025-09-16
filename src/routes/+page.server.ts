63d6a1385c3f60290364ca8a8db28d86a151e79f
Alexander Neville <git@afneville.com>
2025-09-16 10:48:12 +0100

    fetch thumbnials on main page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,13 @@
 
 import { caller } from '$lib/trpc-caller';
+import { IMAGE_DOMAIN, PHOTO_GALLERY_ID } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async (event) => {
+export const load: PageServerLoad = async () => {
   const photoArrays = await caller.getAllItems({});
   return {
-    photoArrays
+    photoArrays,
+    imageDomain: IMAGE_DOMAIN,
+    galleryId: PHOTO_GALLERY_ID
   };
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
-  import type { PageData } from './$types';
+	import type { PhotoArray } from '$lib/api-types';
+	import type { PageData } from './$types';
 
-  export let data: PageData;
+	export let data: PageData;
+
+	function getThumbnailUrl(photoArray: PhotoArray) {
+		if (photoArray.photoUris && photoArray.photoUris.length > 0) {
+			return `${data.imageDomain}/${data.galleryId}/${photoArray.photoArrayId}/${photoArray.photoUris[0]}/thumbnail`;
+		}
+		return '';
+	}
 </script>
 
 <h1>Photo IDs</h1>
 
 {#if data.photoArrays.length}
-  {#each data.photoArrays as photo}
-    <p>{photo.photoArrayId}</p>
-  {/each}
+	{#each data.photoArrays as photo}
+		<div>
+			<p>Photo Array ID: {photo.photoArrayId}</p>
+			{#if getThumbnailUrl(photo)}
+				<img src={getThumbnailUrl(photo)} alt="Thumbnail for {photo.photoArrayId}" />
+			{/if}
+		</div>
+	{/each}
 {:else}
-  <p>No photos found.</p>
+	<p>No photos found.</p>
 {/if}
